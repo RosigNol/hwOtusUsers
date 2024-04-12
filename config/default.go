b3d08ea1a74028395d4bb567b7067f2e747b5eba
rosignol <rosignol.8@gmail.com>
2024-04-12 14:53:19 +0300

    Start next hw

--- config/default.go
+++ config/default.go
@@ -4,7 +4,7 @@ import (
 	"os"
 	// "log"
 	// "fmt"
-	// "github.com/joho/godotenv"
+	//"github.com/joho/godotenv"
 )
 
 type Config struct {
@@ -16,15 +16,25 @@ type Config struct {
 	Password string "POSTGRES_PASSWORD"
 	MigrationsSource string "MIGRATIONS_SOURCE"
 	Port string "PORT"
+	Origin string "ORIGIN"
+
+	AccessTokenPrivateKey  string        "ACCESS_TOKEN_PRIVATE_KEY"
+	AccessTokenPublicKey   string        "ACCESS_TOKEN_PUBLIC_KEY"
+	RefreshTokenPrivateKey string        "REFRESH_TOKEN_PRIVATE_KEY"
+	RefreshTokenPublicKey  string        "REFRESH_TOKEN_PUBLIC_KEY"
+	AccessTokenExpiresIn   time.Duration "ACCESS_TOKEN_EXPIRED_IN"
+	RefreshTokenExpiresIn  time.Duration "REFRESH_TOKEN_EXPIRED_IN"
+	AccessTokenMaxAge      int           "ACCESS_TOKEN_MAXAGE"
+	RefreshTokenMaxAge     int           "REFRESH_TOKEN_MAXAGE"
 }
 
 func LoadConfig(path string) (config Config, err error) {
-	// err = godotenv.Load()
-	// if err != nil {
-	// 	log.Fatalf("Error getting env, %v", err)
-	// } else {
-	// 	fmt.Println("We are getting values")
-	// }
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error getting env, %v", err)
+	} else {
+		fmt.Println("We are getting values")
+	}
 	return Config{
 		PostgreDriver:  os.Getenv("POSTGRES_DRIVER"),
 		Host: os.Getenv("POSTGRES_HOST"),
@@ -33,6 +43,14 @@ func LoadConfig(path string) (config Config, err error) {
 		DBName: os.Getenv("POSTGRES_DB"),
 		Password: os.Getenv("POSTGRES_PASSWORD"),
 		MigrationsSource: os.Getenv("MIGRATIONS_SOURCE"),
-		Port: os.Getenv("PORT"),
+		Origin: os.Getenv("ORIGIN"),
+		AccessTokenPrivateKey: os.Getenv("ACCESS_TOKEN_PRIVATE_KEY"),
+		AccessTokenPublicKey: os.Getenv("ACCESS_TOKEN_PUBLIC_KEY"),
+		RefreshTokenPrivateKey: os.Getenv("REFRESH_TOKEN_PRIVATE_KEY"),
+		RefreshTokenPublicKey: os.Getenv("REFRESH_TOKEN_PUBLIC_KEY"),
+		AccessTokenExpiresIn: os.Getenv("ACCESS_TOKEN_EXPIRED_IN"),
+		RefreshTokenExpiresIn: os.Getenv("REFRESH_TOKEN_EXPIRED_IN"),
+		AccessTokenMaxAge: os.Getenv("ACCESS_TOKEN_MAXAGE"),
+		RefreshTokenMaxAge: os.Getenv("REFRESH_TOKEN_MAXAGE"),
 	}, err
 }
